5155d7cf419490c053a1da50e5924ed1a709af9c
Migii0 <adam.rahmi@outlook.fr>
2022-12-03 17:14:04 +0100

    les problèmes

---
+++ terraform/variable.tf
@@ -0,0 +1,16 @@
+variable "region" {
+default = "GRA11"
+type = string
+}
+variable "instance_name" {
+type = string
+default = "backend_eductive21_gra"
+}
+variable "image_name" {
+type = string
+default = "Debian 11"
+}
+variable "flavor_name" {
+type = string
+default = "s1-2"
+}
